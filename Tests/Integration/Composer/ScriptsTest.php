4fd353b2e4e1819415fff3b1f55c672bbf65d1e5
Oliver Klee <oliver@phplist.com>
2017-09-27 13:34:46 +0200

    [FEATURE] Routing configuration

--- Tests/Integration/Composer/ScriptsTest.php
+++ Tests/Integration/Composer/ScriptsTest.php
@@ -124,4 +124,20 @@ class ScriptsTest extends TestCase
 
         self::assertContains($bundleClassName, $fileContents);
     }
+
+    /**
+     * @return string
+     */
+    private function getModuleRoutesConfigurationFilePath(): string
+    {
+        return dirname(__DIR__, 3) . '/Configuration/routing_modules.yml';
+    }
+
+    /**
+     * @test
+     */
+    public function moduleRoutesConfigurationFileExists()
+    {
+        self::assertFileExists($this->getModuleRoutesConfigurationFilePath());
+    }
 }
