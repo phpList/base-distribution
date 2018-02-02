f6b513c490f6e92f2a6c6ac716441beeed0d235a
Oliver Klee <github@oliverklee.de>
2018-02-02 18:32:13 +0100

    [FEATURE] System tests for the test and dev environment (#69)

---
+++ Tests/System/HttpEndpoints/WebFrontEndTest.php
@@ -0,0 +1,72 @@
+<?php
+declare(strict_types=1);
+
+namespace PhpList\BaseDistribution\Tests\System\HttpEndpoints;
+
+use GuzzleHttp\Client;
+use PhpList\PhpList4\TestingSupport\Traits\SymfonyServerTrait;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Testcase.
+ *
+ * @author Oliver Klee <oliver@phplist.com>
+ */
+class WebFrontEndTest extends TestCase
+{
+    use SymfonyServerTrait;
+
+    /**
+     * @var Client
+     */
+    private $httpClient = null;
+
+    protected function setUp()
+    {
+        $this->httpClient = new Client(['http_errors' => false]);
+    }
+
+    protected function tearDown()
+    {
+        $this->stopSymfonyServer();
+    }
+
+    /**
+     * @return string[][]
+     */
+    public function environmentDataProvider(): array
+    {
+        return [
+            'test' => ['test'],
+            'dev' => ['dev'],
+        ];
+    }
+
+    /**
+     * @test
+     * @param string $environment
+     * @dataProvider environmentDataProvider
+     */
+    public function homepageReturnsSuccess(string $environment)
+    {
+        $this->startSymfonyServer($environment);
+
+        $response = $this->httpClient->get('/', ['base_uri' => $this->getBaseUrl()]);
+
+        self::assertSame(200, $response->getStatusCode());
+    }
+
+    /**
+     * @test
+     * @param string $environment
+     * @dataProvider environmentDataProvider
+     */
+    public function homepageReturnsContent(string $environment)
+    {
+        $this->startSymfonyServer($environment);
+
+        $response = $this->httpClient->get('/', ['base_uri' => $this->getBaseUrl()]);
+
+        self::assertNotEmpty($response->getBody()->getContents());
+    }
+}
