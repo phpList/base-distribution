c96d2348fa3f0e99fa12cebdb8dcd241bc5e7ecb
Xheni Myrtaj <myrtajxheni@gmail.com>
2019-03-07 16:56:33 +0100

    Add db connection for secret value
    Signed-off-by: Xheni Myrtaj <myrtajxheni@gmail.com>

--- config/parameters.php
+++ config/parameters.php
@@ -5,11 +5,27 @@
 
 include_once __DIR__.'/../../config/config.php';
 
+$charset = 'utf8mb4';
+$dsn = "mysql:host=$database_host;dbname=$database_name;charset=$charset";
+$options = array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+);
+try {
+    $pdo = new PDO($dsn, $database_user, $database_password, $options);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+$statement = $pdo->prepare("select value from phplist_config where value = :value");
+$statement->execute(array(':value' => "secret"));
+$row = $statement->fetch();
+
 $container->setParameter('database_host', $database_host);
 $container->setParameter('database_port', $database_port);
 $container->setParameter('database_name', $database_name);
 $container->setParameter('database_user', $database_user);
 $container->setParameter('database_password', $database_password);
-$container->setParameter('database_path', null);
+$container->setParameter('database_path', $table_prefix);
 $container->setParameter('database_driver','pdo_mysql');
-$container->setParameter('secret', 'be0652de9881338f9e597fd8fa459ad09b422dae');
+$container->setParameter('secret', $row);
